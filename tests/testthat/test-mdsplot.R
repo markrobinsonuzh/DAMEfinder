768c8b3baf355a3c104f4973c234201dd084bb84
sorjuela <sorjuela@gmail.com>
2019-03-29 17:34:29 +0100

    fix tests to accomodate change in split_bams output

--- tests/testthat/test-mdsplot.R
+++ tests/testthat/test-mdsplot.R
@@ -4,9 +4,9 @@ DATA_PATH_DIR <- system.file("extdata", ".", package = "DAMEfinder")
 
 get_data_path <- function(file_name) file.path(DATA_PATH_DIR, file_name)
 
-rds_files <- list.files(DATA_PATH_DIR, ".rds$")
-rds_files <- get_data_path(rds_files)
-derASM <- calc_derivedasm(rds_files, cores = 1, verbose = T)
+sample_list <- try(readRDS(get_data_path("splitbams_output.rds")))
+
+derASM <- calc_derivedasm(sample_list, cores = 1, verbose = T)
 
 test_that("end to end mdsplot", {
   p <- methyl_MDS_plot(derASM, color = c(rep("CRC",4), rep("NORM", 4)))
