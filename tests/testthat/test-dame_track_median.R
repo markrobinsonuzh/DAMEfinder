e8abf89fb852465b15283e2000fb77dbcb4f5c2c
sorjuela <sorjuela@gmail.com>
2019-09-20 16:54:23 +0200

    add new dame_track_median function

---
+++ tests/testthat/test-dame_track_median.R
@@ -0,0 +1,24 @@
+context("test-dame_track_median")
+library(GenomicRanges)
+
+data(extractbams_output)
+
+derASM <- calc_derivedasm(extractbams_output, cores = 1, verbose = TRUE)
+DAME <- GRanges(19, IRanges(306443,310272))
+
+SummarizedExperiment::colData(derASM)$group <- c(rep("CRC",4), rep("NORM", 4))
+
+test_that("SNP dame track", {
+  p <- dame_track_median(dame = DAME, derASM = derASM)
+  expect_true(is.ggplot(p))
+})
+
+data(readtuples_output)
+ASM <- calc_asm(readtuples_output)
+SummarizedExperiment::colData(ASM)$group <- c(rep("CRC",3), rep("NORM", 2))
+SummarizedExperiment::colData(ASM)$samples <- colnames(ASM)
+
+test_that("dame track with both", {
+  p <- dame_track_median(dame = DAME, derASM = derASM, ASM = ASM)
+  expect_true(is.ggplot(p))
+})
